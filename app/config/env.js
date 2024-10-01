72495625a50c022d979cf32c9652d979705617a3
KevinCay <kcayc@miumg.edu.gt>
2024-10-01 11:42:52 -0600

    actualizacion

--- app/config/env.js
+++ app/config/env.js
@@ -1,9 +1,9 @@
 
 const env = {
-  database: 'antigua_umg2024_2103_dfcc',
+  database: 'antigua_umg2024_2103_dfcc_u3qj',
   username: 'antigua_umg2024_2103_dfcc_user',
-  password: 'Z7gSwoUTsXz2ZfaXjO4hwheHNcytzn8O',
-  host: 'dpg-cr6jnhlds78s73bur49g-a.oregon-postgres.render.com',
+  password: 'kziBqHpntBtjHEyX76dPDVTiWTT9b2Je',
+  host: 'dpg-cru3732j1k6c73bm1je0-a',
   dialect: 'postgres',
   pool: {
     max: 5,
